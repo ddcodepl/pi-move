b632b0c9e28c059b238b1824d6a22d442a1f5363
Damian Dabrowski <dabek1812@gmail.com>
2019-07-04 04:03:17 +0200

    Add option to recognize location

--- Movement.py
+++ Movement.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python
-import telegram
+import os
+import time
+import config
 import subprocess
 import RPi.GPIO as GPIO
-import time
-import pymongo
-import config
-import os
-from Spotify import switch_device_and_play_music
 
-SENSOR_PIN = config.device.move_sensor_port
+device = config.getDeviceConfig()
 
+# # Sensor Settings
+SENSOR_PIN = config.movement_sensor_pin
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
 
-# DB Config
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["movement_tracker"]
 
-# Telegram Config
-telegram = telegram.Bot(config.telegram_token)
-chat_id = config.telegram_chat_id
-
-# Actions list
-# # 1 - Move detected
-# # 2 - Move detected and photo taken
-# # 3 - Movement detected and Spotify activated
-# # 4 - Movement detected, photo taken and Spotify activated
-
-# Turn off after X seconds
-
-# define our clear function
 def clear():
     # for windows
     if os.name == 'nt':
@@ -37,58 +20,29 @@ def clear():
     else:
         _ = os.system('clear')
 
-def save_record(location=config.device.id, action=1):
-    logs = db.logs
-    data = {
-        "location_id": location,
-        "action_id": action,
-        "time": int(time.time())
-    }
-    insert = logs.insert_one(data)
-
 
 def move_cb(channel):
-    actionID = 1
-
-    if config.device.has_speaker == 1:
-        switch_device_and_play_music()
-        actionID = 3
-
-    save_record(config.device.id, actionID)
-
-    if telegram.send_message(chat_id=chat_id, text="Movement detected in attic"):
+    if config.telegram.send_message(chat_id=config.telegram_chat_id, text="Movement detected in "+device['location']['name']):
         print('message sent')
 
 
 def photo_cb(channel):
-    actionID = 2
-
-    if config.device.has_speaker == 1:
-        switch_device_and_play_music()
-        actionID = 4
-
-    subprocess.call('./photo.sh', shell=True)
-    clear()
-
-    if telegram.send_photo(chat_id=chat_id, photo=open('./snap.jpg', 'rb')) :
+    if config.telegram.send_photo(chat_id=config.telegram_chat_id, photo=open('./snap.jpg', 'rb')):
         print('photo sent')
 
-    if telegram.send_message(chat_id=chat_id, text="Movement detected in the "+config.device.location):
+    if config.telegram.send_message(chat_id=config.telegram_chat_id, text="Movement detected in "+device['location']['name']):
         print('message sent')
-
-    save_record(config.device.id, actionID)
 
 
 time.sleep(2)
 
 try:
-
-    if config.device.has_camera == 0:
-        print("Init without camera")
-        GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=move_cb)
-    else:
+    if device['has_camera']:
         print("Init with camera")
         GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=photo_cb)
+    else:
+        print("Init without camera")
+        GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=move_cb)
 
     while 1:
         time.sleep(1)
---
+++ README.md
@@ -0,0 +1,27 @@
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install telegram-telegram-bot pymongo spotipy
+```
+
+### Software requirements
+
+```bash
+Python *
+MongoDB *
+TelegramBot *
+Spotify account
+```
+
+### Hardware requirements
+
+```bash
+Raspberry Pi *
+Pi Movement sensor *
+Camera
+Speaker
+```
+
+`* Required`
